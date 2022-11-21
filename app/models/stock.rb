15bc379f7d8ccbf902896d480825e8c87a03b8cd
Yoji Sekiya <yojisekiya@gmail.com>
2022-11-21 23:49:19 +0900

    add edit user profile feature

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
+
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:publishable_token],
@@ -11,5 +16,9 @@ class Stock < ApplicationRecord
     rescue StandardError => e
       nil
     end
+  end
+
+  def self.check_db(ticker_symbol)
+    Stock.where(ticker: ticker_symbol).first
   end
 end
