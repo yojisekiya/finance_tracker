1df0954cf9e252177a5f85e0eb41631168485047
Yoji Sekiya <yojisekiya@gmail.com>
2022-11-27 11:19:38 +0900

    add search friend funtion etc.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: %i[create destroy]
+  resources :users, only: %i[show]
 end
